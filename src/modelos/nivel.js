36e9d4a240cebdb62fe771d8720942e56ff59e76
Sergio <sergioriverasalgado15@gmail.com>
2023-03-09 11:54:31 +0100

    Niveles actualizado

--- src/modelos/nivel.js
+++ src/modelos/nivel.js
@@ -6,5 +6,29 @@ const nivel =[
         numPiezas:"4",
         x:437,
         y:310
+    },
+    {
+        nivel:2,
+        texto:"Soy un texto ignaciano2",
+        imagen:"../assets/img/ignacio2.png",
+        numPiezas:"4",
+        x:437,
+        y:310
+    },
+    {
+        nivel:3,
+        texto:"Soy un texto ignaciano3",
+        imagen:"../assets/img/ignacio3.png",
+        numPiezas:"4",
+        x:437,
+        y:310
+    },
+    {
+        nivel:4,
+        texto:"Soy un texto ignaciano4",
+        imagen:"../assets/img/ignacio4.png",
+        numPiezas:"4",
+        x:437,
+        y:310
     }
 ]
