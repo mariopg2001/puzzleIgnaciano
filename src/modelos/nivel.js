c40207d16c47edf564aa60f6f70977534c411611
lauramerinoortiz <lauramerinoortiz.guadalupe@alumnado.fundacionloyola.net>
2023-03-10 08:59:30 +0100

    Niveles ok faltan medidas, img subidas

--- src/modelos/nivel.js
+++ src/modelos/nivel.js
@@ -12,23 +12,119 @@ const nivel =[
         texto:"Soy un texto ignaciano2",
         imagen:"../assets/img/ignacio2.png",
         numPiezas:"4",
-        x:437,
-        y:310
+        x:435,
+        y:306
     },
     {
         nivel:3,
         texto:"Soy un texto ignaciano3",
         imagen:"../assets/img/ignacio3.png",
         numPiezas:"4",
-        x:437,
-        y:310
+        x:444,
+        y:308
     },
     {
         nivel:4,
         texto:"Soy un texto ignaciano4",
         imagen:"../assets/img/ignacio4.png",
         numPiezas:"4",
-        x:437,
-        y:310
+        x:434,
+        y:311
+    },
+    {
+        nivel:5,
+        texto:"Soy un texto ignaciano5",
+        imagen:"../assets/img/ignacio5.png",
+        numPiezas:"9",
+        x:300,
+        y:203
+    },
+    {
+        nivel:6,
+        texto:"Soy un texto ignaciano6",
+        imagen:"../assets/img/ignacio6.png",
+        numPiezas:"9",
+        x:300,
+        y:203
+    },
+    {
+        nivel:7,
+        texto:"Soy un texto ignaciano7",
+        imagen:"../assets/img/ignacio7.png",
+        numPiezas:"9",
+        x:111,
+        y:111
+    },
+    {
+        nivel:8,
+        texto:"Soy un texto ignaciano8",
+        imagen:"../assets/img/ignacio8.png",
+        numPiezas:"9",
+        x:111,
+        y:111
+    },
+    {
+        nivel:9,
+        texto:"En 1528 San Ignacio entró en la Universidad de París, donde estuvo más de 7 años, para aumentar su educación teológica y literaria, tratando de enseñar a los estudiantes sus Ejercicios Espirituales. Los Ejercicios Espirituales es un libro con oraciones y meditaciones para hacer durante 30 días y así acercar a la persona hacia Dios.",
+        imagen:"../assets/img/ignacio9.png",
+        numPiezas:"16",
+        x:219,
+        y:152
+    },
+    {
+        nivel:10,
+        texto:"Soy un texto ignaciano10",
+        imagen:"../assets/img/ignacio10.png",
+        numPiezas:"16",
+        x:227,
+        y:154
+    },
+    {
+        nivel:11,
+        texto:"Soy un texto ignaciano11",
+        imagen:"../assets/img/ignacio11.png",
+        numPiezas:"16",
+        x:111,
+        y:111
+    },
+    {
+        nivel:12,
+        texto:"Soy un texto ignaciano12",
+        imagen:"../assets/img/ignacio12.png",
+        numPiezas:"16",
+        x:111,
+        y:111
+    },
+    {
+        nivel:13,
+        texto:"Soy un texto ignaciano13",
+        imagen:"../assets/img/ignacio13.png",
+        numPiezas:"25",
+        x:111,
+        y:111
+    },
+    {
+        nivel:14,
+        texto:"Soy un texto ignaciano14",
+        imagen:"../assets/img/ignacio14.png",
+        numPiezas:"25",
+        x:111,
+        y:111
+    },
+    {
+        nivel:15,
+        texto:"Soy un texto ignaciano15",
+        imagen:"../assets/img/ignacio15.png",
+        numPiezas:"25",
+        x:111,
+        y:111
+    },
+    {
+        nivel:16,
+        texto:"Soy un texto ignaciano16",
+        imagen:"../assets/img/ignacio16.png",
+        numPiezas:"25",
+        x:111,
+        y:111
     }
 ]
